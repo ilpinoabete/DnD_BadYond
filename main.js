36fbac0367af07eb20438b8ba50d6b7966b5a125
ilpinoabete <cristianotollot@gmail.com>
2023-12-08 11:48:49 +0100

    Added multi class support

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ function toggleMenubar(){
     menu.style.display = menu.style.display === "none" ? "grid" : "none";
 }
 
+function displayClasses(classes, index){
+    document.getElementById("main-title").querySelector("h1").innerHTML = classes[index].id;
+
+    //hiding all classes
+    classes.forEach((e) => {
+        document.getElementById(e.id).style.display = "none";
+    })
+
+    //displaying the selected class
+    document.getElementById(classes[index].id).style.display = "block";
+}
+
 const main = () => {
+    //global variables
+    const classes = Array.from(document.querySelectorAll(".class"));
+    var index = 0;
+
     //menu toggle
     Array.from(document.getElementsByClassName("menu-toggle")).forEach((e) => {
         e.addEventListener("click", () => {
@@ -32,7 +48,23 @@ const main = () => {
         })
     })
 
+    //Display buttons
+    document.getElementById("button-next").addEventListener("click", () => {
+        //changing index value
+        index >= (classes.length - 1) ? index=0 : index++; 
+
+        displayClasses(classes, index);
+    })
+
+    document.getElementById("button-previous").addEventListener("click", () => {
+        //changing index value
+        index <= 0 ? index= (classes.length - 1) : index--; 
+
+        displayClasses(classes, index);
+    })
+
     toggleMenubar();
+    displayClasses(classes, index);
 }
 
 main()
